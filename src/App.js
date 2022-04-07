05b61448789664224e8c7bba68b403ebcb6127e6
Gonçalo Estrelado <goncalo.estrelado@hotmail.com>
2022-04-07 19:24:18 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
-import { Navbar } from "./components";
+import { Footer, Navbar } from "./components";
 
 function App() {
   return (
     <div className={styles.textColor}>
       <Navbar />
       <Outlet />
-      footer
+      <Footer />
     </div>
   );
 }
