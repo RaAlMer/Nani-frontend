4451aa5759f6bdb4f16b6ba0997cd274dfc75776
Gonçalo Estrelado <goncalo.estrelado@hotmail.com>
2022-04-28 23:04:13 +0100

    Added Conditional alert for Add Anime List dropdown

--- src/pages/Anime/index.js
+++ src/pages/Anime/index.js
@@ -1,5 +1,5 @@
 import styles from "./Anime.module.scss";
-import { AnimeComponent, ListOfComments, CommentForm } from "../../components";
+import { AnimeComponent, ListOfComments } from "../../components";
 import { useParams } from "react-router-dom";
 import { AnimeDropdown } from "components/AnimeDropdown";
 import { useContext, useEffect, useState } from "react";
@@ -8,16 +8,27 @@ import { Alert } from "../../components";
 import { AuthContext } from "context";
 
 export function Anime() {
-  const { user } = useContext(AuthContext);
+  const { user, getUser } = useContext(AuthContext);
   const { animeId } = useParams();
   const [addListStatus, setAddListStatus] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    let timeout;
+    if (showAlert) {
+      timeout = setTimeout(() => setShowAlert(false), 2000);
+    }
+    return () => clearTimeout(timeout);
+  }, [showAlert]);
 
   const addWatched = async () => {
     try {
       const item = await client.get(`/anime/addList/${animeId}/watched`);
       if (item.status === 200) {
-        setAddListStatus(true);
+        getUser();
       }
+      setAddListStatus(item.data);
+      setShowAlert(true);
     } catch (error) {
       console.log(error);
     }
@@ -26,8 +37,10 @@ export function Anime() {
     try {
       const item = await client.get(`/anime/addList/${animeId}/watching`);
       if (item.status === 200) {
-        setAddListStatus(true);
+        getUser();
       }
+      setAddListStatus(item.data);
+      setShowAlert(true);
     } catch (error) {
       console.log(error);
     }
@@ -36,8 +49,10 @@ export function Anime() {
     try {
       const item = await client.get(`/anime/addList/${animeId}/planToWatch`);
       if (item.status === 200) {
-        setAddListStatus(true);
+        getUser();
       }
+      setAddListStatus(item.data);
+      setShowAlert(true);
     } catch (error) {
       console.log(error);
     }
@@ -46,18 +61,26 @@ export function Anime() {
   return (
     <>
       <div>
-        <h1>Anime</h1>
-        <AnimeComponent id={animeId} type="large" />
-        <AnimeDropdown
-          addWatched={addWatched}
-          addWatching={addWatching}
-          addPlanToWatch={addPlanToWatch}
-        />
-        {addListStatus && (
-          <Alert type="success" message="Added to list">
-            <h3>Successfully added to your List</h3>
-          </Alert>
-        )}
+        <div className={styles.container}>
+          <AnimeComponent id={animeId} type="large" />
+          <AnimeDropdown
+            addWatched={addWatched}
+            addWatching={addWatching}
+            addPlanToWatch={addPlanToWatch}
+          />
+          <div className={styles.alert}>
+            {showAlert &&
+              (addListStatus ? (
+                <Alert type="warning" message="Added to list">
+                  <h3>Already in this list</h3>
+                </Alert>
+              ) : (
+                <Alert type="success" message="Added to list">
+                  <h3>Successfully added to your List</h3>
+                </Alert>
+              ))}
+          </div>
+        </div>
         <br />
         <ListOfComments
           currentUserId={user ? user._id : null}
