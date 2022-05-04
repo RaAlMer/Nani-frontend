2dc3fa7d9732b013bed23cc78e55d431387a4926
Raúl <raul.alonso.merino@gmail.com>
2022-05-05 00:09:53 +0200

    Removed unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { Navbar } from "./Navbar";
 export { Footer } from "./Footer";
-export { Notificationbar } from "./Notificationbar";
 export { Signup } from "./Signup";
 export { Login } from "./Login";
 export { PrivateRoute } from "./PrivateRoute";
